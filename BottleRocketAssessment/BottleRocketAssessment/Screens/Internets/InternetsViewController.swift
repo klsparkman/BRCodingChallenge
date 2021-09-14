8619cdacabd458f293ca4f44e5875085d07da771
Kelsey Sparkman <kelseysparkman14@gmail.com>
2021-09-13 20:55:57 -0600

    Add viewcontrollers to screens and mainTabBarController

---
+++ BottleRocketAssessment/BottleRocketAssessment/Screens/Internets/InternetsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InternetsViewController.swift
+//  BottleRocketAssessment
+//
+//  Created by Kelsey Sparkman on 9/13/21.
+//
+
+import UIKit
+
+class InternetsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BottleRocketAssessment/BottleRocketAssessment/Shared/Controllers/MainTabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabBarController.swift
+//  BottleRocketAssessment
+//
+//  Created by Kelsey Sparkman on 9/13/21.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+}
